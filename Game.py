6f52922cfcb999f7571dd3f350db6101f1cec6b0
cRohda <cormacrohda@gmail.com>
2021-10-20 13:25:05 -0400

    Window makes a placebo virus

--- Game.py
+++ Game.py
@@ -1,8 +1,30 @@
-from tkinter import *
-import time
+import tkinter as tk
 
-window = Tk()
-window.title('Clicker Game')
-window.configure(width=1000, height=600)
-window.configure(bg='lightgray')
-window.mainloop()
+while True:
+    window = tk.Tk()
+    window.title('You are infected')
+    window.configure(width=500, heigh=300)
+    window.configure(bg='red')
+    label = tk.Label(
+        text='you are infected with many virus',
+        fg='black',
+        bg='red',
+        width=1000,
+        height=20
+    )
+    CCN = tk.Label(
+        text='enter your credit card number to clean your computer:',
+        fg='black',
+        bg='red',
+        width=500,
+        height=3
+    )
+    CC = tk.Entry(
+        bg='white',
+        fg='black',
+        width=500
+    )
+    label.pack()
+    CCN.pack()
+    CC.pack()
+    window.mainloop()
