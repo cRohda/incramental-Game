5f3459290e8abd359bf9d9194cb4b3c165bd8228
cRohda <cormacrohda@gmail.com>
2021-10-24 09:38:51 -0400

    Added sprite

--- Game.py
+++ Game.py
@@ -2,36 +2,14 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title('Address Entry Form')
+window.title('sprites test')
+mlabel = tk.Label(text='Sprites Test', fg='yellow', bg='green').pack()
 
-frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
-frm_form.pack()
+canvas = tk.Canvas(window, width=1000, height=1000, bg='white')
 
-# First, last, Add1, Add2, City, State, Code, Country
-labels = [
-    'First Name:',
-    'Last Name:',
-    'Address Line 1:',
-    'Address Line 2:',
-    'City',
-    'State/Province',
-    'Postal Code',
-    'Country',
-]
+gif1 = tk.PhotoImage(file='sprite.gif')
+id1 = canvas.create_image(500, 500, image=gif1)
+canvas.pack(padx=10, pady=10)
 
-for idx, text in enumerate(labels):
-    label = tk.Label(master=frm_form, text=text)
-    entry = tk.Entry(master=frm_form, width=50)
-    label.grid(row=idx, column=0, sticky='e')
-    entry.grid(row=idx, column=1)
-
-frm_buttons = tk.Frame()
-frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
-
-btn_submit = tk.Button(master=frm_buttons, text='Submit')
-btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
-
-btn_clear = tk.Button(master=frm_buttons, text='Clear')
-btn_clear.pack(side=tk.RIGHT, padx=10, ipadx=10)
-
+canvas.mainloop()
 window.mainloop()
