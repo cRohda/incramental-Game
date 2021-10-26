5951bf7eb68a1063c4f09cb097b7667702fbd37b
cRohda <cormacrohda@gmail.com>
2021-10-26 09:04:41 -0400

    Added autocliker button, no function yet

--- Game.py
+++ Game.py
@@ -1,33 +1,42 @@
 # https://realpython.com/python-gui-tkinter/#check-your-understanding_1
 import tkinter as tk
 import webbrowser
+import time
 
 def click():
     value = int(money["text"])
     money["text"] = f"{value + 1}"
 
-def rickroll()
+def rickroll():
     url = 'https://www.youtube.com/watch?v=x31tDT-4fQw'
     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
     webbrowser.get(chrome_path).open(url)
+
+def p1():
+    print('Autoclicker')
 
 window = tk.Tk()
 window.title('BC High Clicker')
 window.configure(bg='white')
 
+# Images
 picture = tk.PhotoImage(file='Logo.png')
 tutorial = tk.PhotoImage(file='Instructions.png')
 price = tk.PhotoImage(file='Price.png')
+mouse = tk.PhotoImage(file='Power1.png')
 
+# Labels and Buttons
 instructions = tk.Label(image=tutorial)
 btn = tk.Button(image=picture, command=click)
 money = tk.Label(text='0',bg='white', fg='black', font='verdana')
 sign = tk.Label(image=price)
+P1 = tk.Button(image=mouse, bg='white', command=p1)
 
+# Griding
 instructions.grid(row=0, column=0, sticky='w')
-btn.grid(row=1, column=0, sticky='nsew')
+btn.grid(row=2, column=0, sticky='w')
 sign.grid(row=0, column=1)
-money.grid(row=1, column=1)
-
+money.grid(row=0, column=3)
+P1.grid(row=3, column=0, sticky='w')
 
 window.mainloop()
