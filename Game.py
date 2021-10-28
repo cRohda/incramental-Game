233ea5f817b2d2853dbaf3ccfcf878a64eaaf825
cRohda <cormacrohda@gmail.com>
2021-10-28 12:36:33 -0400

    Rebranded into PizzaClicker Added pps, and amount of each power-up.

--- Game.py
+++ Game.py
@@ -4,8 +4,8 @@ from time import sleep
 
 
 def click():
-    value = int(money["text"])
-    money["text"] = f"{value + 1}"
+    value = float(wealth["text"])
+    wealth["text"] = f"{value + 1}"
 
 def rickroll():
     url = 'https://www.youtube.com/watch?v=x31tDT-4fQw'
@@ -13,37 +13,53 @@ def rickroll():
     webbrowser.get(chrome_path).open(url)
 
 def p1():
-    if int(money['text']) >= 50:
-        p1loss = int(money['text'])
-        money['text'] = f'{p1loss - 50}'
-        while True:
-            acvalue = int(money['text'])
-            money['text'] = f'{acvalue + 1}'
-            sleep(5)
+    if float(wealth['text']) >= 40:
+        p1loss = float(wealth['text'])
+        wealth['text'] = f'{p1loss - 40}'
+
+        ac_count = float(acamount['text'])
+        acamount['text'] = f'{ac_count + 1}'
+
+        ac_cps = float(pps['text'])
+        pps['text'] = f'{ac_cps + 0.25}'
+    else:
+        overdraft()
+
+def overdraft():
+    print('Not enough money!')
 
 
 window = tk.Tk()
-window.title('BC High Clicker')
+window.title('Click The Pizza 3000')
+window.geometry("800x1000")
 window.configure(bg='white')
 
 # Images
 picture = tk.PhotoImage(file='Logo.png')
-tutorial = tk.PhotoImage(file='Instructions.png')
+header = tk.PhotoImage(file='Title.png')
 price = tk.PhotoImage(file='Price.png')
 mouse = tk.PhotoImage(file='Power1.png')
 
-# Labels and Buttons
-instructions = tk.Label(image=tutorial)
-btn = tk.Button(image=picture, command=click)
-money = tk.Label(text='0',bg='white', fg='black', font='verdana')
-sign = tk.Label(image=price)
-P1 = tk.Button(image=mouse, bg='white', command=p1)
+# Labels
+title = tk.Label(image=header, bg='white')
+space = tk.Label(text="\n\n\n\n", bg='white')
+sign = tk.Label(image=price, bg='white')
+wealth = tk.Label(text='200', font=('Verdana', 69), bg='white', fg='black')
+pps = tk.Label(text='0', font=('Verdana', 40), bg='white', fg='black')
+acamount = tk.Label(text='0', font=('Verdana', 40), bg='white', fg='black')
+
+# Buttons
+logo = tk.Button(image=picture, command=click)
+autoclick = tk.Button(image=mouse, bg='white', command=p1)
 
 # Griding
-instructions.grid(row=0, column=0, sticky='w')
-btn.grid(row=2, column=0, sticky='w')
-sign.grid(row=0, column=1)
-money.grid(row=0, column=3)
-P1.grid(row=3, column=0, sticky='w')
+title.grid(row=0, column=0)
+pps.grid(row=1, column=0, sticky='n')
+logo.grid(row=2, column=0, sticky='nsew')
+sign.grid(row=1, column=1, sticky='e')
+wealth.grid(row=1, column=2, sticky='w')
+space.grid(row=3, column=0)
+autoclick.grid(row=4, column=0, sticky='e')
+acamount.grid(row=4, column=1, sticky='w')
 
 window.mainloop()
